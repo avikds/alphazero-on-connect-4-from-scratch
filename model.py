7d27735c07fc5990c5c2a2efe0ac51b3a73751b0
Avik Das <avikd957@gmail.com>
2026-09-03 17:30:44 +0530

    Step 43: value_loss_mse

--- model.py
+++ model.py
@@ -620,8 +620,10 @@ def generate_self_play_batch(net, num_games, num_simulations, c_puct, temperatur
 
     return buffer
 
-# Step 43 - value_loss_mse (not yet solved)
-# TODO: implement
+# Step 43 - value_loss_mse
+def value_loss_mse(predicted_values, target_values):
+    """Return the mean squared error between predicted and target values."""
+    return torch.mean((predicted_values - target_values) ** 2)
 
 # Step 44 - policy_loss_cross_entropy (not yet solved)
 # TODO: implement
