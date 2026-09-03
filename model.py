109f2995274274507c68f7e989e3e4b39409495c
Avik Das <avikd957@gmail.com>
2026-09-03 17:25:36 +0530

    Step 27: make_mcts_node

--- model.py
+++ model.py
@@ -434,8 +434,28 @@ def backup_value(leaf, value):
         current_value = -current_value
         node = node["parent"]
 
-# Step 35 - run_one_simulation (not yet solved)
-# TODO: implement
+# Step 35 - run_one_simulation
+def run_one_simulation(root, net, c_puct):
+    """Run one full PUCT MCTS simulation from the root."""
+    leaf = select_leaf(root, c_puct)
+
+    board = leaf["board"]
+    to_play = leaf["to_play"]
+
+    done, winner = is_terminal(board)
+
+    if done:
+        if winner == 0:
+            value = 0.0
+        elif winner == to_play:
+            value = 1.0
+        else:
+            value = -1.0
+    else:
+        priors, value = evaluate_with_network(net, board, to_play)
+        expand_node(leaf, priors)
+
+    backup_value(leaf, value)
 
 # Step 36 - run_mcts (not yet solved)
 # TODO: implement
